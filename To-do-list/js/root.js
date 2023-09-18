d4dabf007ade49a2327223440299d6beb07928b5
Dino27-bat <107429990+Dino27-bat@users.noreply.github.com>
2023-09-18 15:05:47 -0300

    Add files via upload

---
+++ To-do-list/js/root.js
@@ -0,0 +1,7 @@
+let banco = [
+    {'nome': 'Pedro'} // JSON
+];
+
+localStorage.setItem('dia da semana', 'Segunda-feira');
+localStorage.setItem("banco", JSON.stringify(banco));
+localStorage.setItem()
